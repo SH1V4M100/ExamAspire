788b7c8b34021ea70bf4d08d4b52340dfdd72848
Shivam <shivamchatterjee471@gmail.com>
2025-12-19 13:33:00 +0530

    updated next js version

--- src/app/(frontend)/next/preview/route.ts
+++ src/app/(frontend)/next/preview/route.ts
@@ -3,21 +3,14 @@ import { getPayload } from 'payload'
 
 import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { NextRequest } from 'next/server'
 
 import configPromise from '@payload-config'
 
-export async function GET(
-  req: {
-    cookies: {
-      get: (name: string) => {
-        value: string
-      }
-    }
-  } & Request,
-): Promise<Response> {
+export async function GET(request: NextRequest): Promise<Response> {
   const payload = await getPayload({ config: configPromise })
 
-  const { searchParams } = new URL(req.url)
+  const { searchParams } = new URL(request.url)
 
   const path = searchParams.get('path')
   const collection = searchParams.get('collection') as CollectionSlug
@@ -40,8 +33,9 @@ export async function GET(
 
   try {
     user = await payload.auth({
-      req: req as unknown as PayloadRequest,
-      headers: req.headers,
+      // Payload still expects a PayloadRequest
+      req: request as unknown as PayloadRequest,
+      headers: request.headers,
     })
   } catch (error) {
     payload.logger.error({ err: error }, 'Error verifying token for live preview')
@@ -55,7 +49,7 @@ export async function GET(
     return new Response('You are not allowed to preview this page', { status: 403 })
   }
 
-  // You can add additional checks here to see if the user is allowed to preview this page
+  // Optional: extra permission checks here
 
   draft.enable()
 
